d37f0f975b65983bdea604375d90f131b5aa0198
Safraop <aakashsangwan214@gmail.com>
2025-05-10 17:47:09 +0530

    marks.py
    it helps u to know how bad you are at your study

--- marks.py
+++ marks.py
@@ -1,15 +1,5 @@
  
-"""f1 = int(input("maths marks : "))
-f2 = int(input("hindi marks : "))
-f3 = int(input("science marks : "))
-f4 = int(input("english marks : "))
-f5 = int(input("sst marks : "))
-f6 = int(input("computer marks : "))
-f7 = int(input("music marks : "))
-a = [f1,f2,f3,f4,f5,f6,f7]
-b = sum(a)
-c = (b/700) * 100
-print(f"here your percentage: {c}")"""
+
 
 
 subjects = ["Maths", "Hindi", "Science", "English", "SST", "Computer", "Music"]
